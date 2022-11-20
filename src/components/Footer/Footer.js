6706f9a62ad6a45a645a8819c3f1245fa5900d36
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-11-20 13:11:35 -1000

    footer componentupdated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin, AiFillFilePdf } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div style={{ backgroundColor: "#EE5B25" }}>
+      <FooterWrapper>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>© All Rights Reserved. 2022 Christine Codes Portfolio.</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/hummingbirdcreative">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/christinegrayb/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://docs.google.com/document/d/1Uc3kXrvCs_Bgv9SRnU3055eph4wS3Mso4BMLicThu6U/edit?usp=sharing">
+          <AiFillFilePdf size="3rem" />
+        </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
     </div>
   );
 };
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,13 +1,12 @@
 import styled from "styled-components"
 
 export const FooterWrapper = styled.section`
-	width: calc(100vw - 96px);
+  
+  width: calc(100vw - 96px);
   max-width: 1040px;
   padding: 2rem 48px 40px;
-  margin: 1rem auto;
+  margin: auto;
   box-sizing: content-box;
-
-
   @media ${props => props.theme.breakpoints.sm} {
     padding: 0 16px 48px;
     width: calc(100vw - 32px);
@@ -22,18 +21,15 @@ export const LinkItem = styled.a`
 	transition: .3s ease;
 	position: relative;
 	left: 0;
-
 	&:hover {
 		color: #fff;
 		left: 6px;
 	}
-
 	@media ${props => props.theme.breakpoints.md} {
 		font-size: 16px;
 		line-height: 28px;
 		display: flex;
 	}
-
 	@media ${props => props.theme.breakpoints.sm} {
 		font-size: 8px;
 		line-height: 14px;
@@ -47,12 +43,10 @@ export const SocialIconsContainer = styled.div`
 max-width: 1040px;
 display: flex;
 justify-content: space-between;
-
 @media ${props => props.theme.breakpoints.md}{
   display: flex;
   justify-content: space-between;
 }
-
 @media ${props => props.theme.breakpoints.sm}{
   display: flex;
 	width: 100%;
@@ -66,12 +60,10 @@ export const CompanyContainer = styled.div`
 	flex-wrap: wrap;
 	margin-right: auto;
 	
-
 	@media ${props => props.theme.breakpoints.md}{
 		flex-direction: column;
 		align-items: baseline;
 	}
-
 	@media ${props => props.theme.breakpoints.sm}{
 		display: flex;
 		flex-direction: column;
@@ -88,12 +80,10 @@ export const Slogan = styled.p`
 	font-size: 18px;
 	line-height: 30px;
 	padding: 1rem;
-
 	@media ${props => props.theme.breakpoints.md}{
 		font-size: 16px;
 		line-height: 28px;
 	}
-
 	@media ${props => props.theme.breakpoints.sm}{
 		line-height: 22px;
 		font-size: 14px;
@@ -104,7 +94,6 @@ export const Slogan = styled.p`
 export const SocialContainer = styled.div`
 	display: flex;
   align-items: center;
-
 	@media ${props => props.theme.breakpoints.md}{
 		justify-content: center;
 		padding-right: 16px;
@@ -119,11 +108,9 @@ export const LinkList = styled.ul`
 	grid-template-columns: repeat(3, minmax(85px, 220px));
 	gap: 40px;
   padding: 40px 0 28px;
-
 	@media ${props => props.theme.breakpoints.lg} {
 		padding: 32px 0 16px;
 	}
-
 	@media ${props => props.theme.breakpoints.md} {
 		width: 100%;
 		padding: 32px 0 16px;
@@ -150,7 +137,6 @@ export const LinkTitle = styled.h4`
 	text-transform: uppercase;
 	color: rgba(255, 255, 255, 0.4);
 	margin-bottom: 16px;
-
 	@media ${props => props.theme.breakpoints.sm} {
 		font-size: 10px;
 		line-height: 12px;
