c9458c434b97156fa91a81849b412dac16029f93
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-11-20 13:01:19 -1000

    app initialized

---
+++ src/themes/default.js
@@ -0,0 +1,15 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Cormorant, serif",
+    main: "Montserrat, sans-serif"
+  },
+
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+}
