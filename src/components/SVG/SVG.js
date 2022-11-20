1fcd16457e54c1cf52f6c1296f10fa34eb2b877a
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-11-20 13:08:33 -1000

    SVG component added

---
+++ src/components/SVG/SVG.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SVG = () => {
+  return (
+   <svg viewBox="0 0 1440 200">
+    <path fill="#Defe7b" fill-rule="evenodd" clip-rule="evenodd" d="M1448 0l-60.62 22.222c-60.63 22.222-181.88 66.667-303.13 72.222C963 100 841.75 66.667 720.5 50S478 33.333 356.75 38.889C235.5 44.444 114.25 55.556 53.625 61.11L-7 66.667V200h1455V0z"></path>
+    </svg>
+  )
+}
+
+export default SVG
